d8dd44c765adc5985a1615f0ad29e525a51dd1fe
Anders Bjerner <abjerner@skybrud.dk>
2023-06-07 09:18:09 +0200

    Fixed bad formatting

--- src/Limbo.FormattingObjects/Tables/FoTableRow.cs
+++ src/Limbo.FormattingObjects/Tables/FoTableRow.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Xml.Linq;
 using Skybrud.Essentials.Strings.Extensions;
 
-namespace Limbo.FormattingObjects.Tables
-{
+namespace Limbo.FormattingObjects.Tables {
 
     /// <summary>
     /// Groups table-cells into rows.
@@ -12,8 +11,7 @@ namespace Limbo.FormattingObjects.Tables
     /// <see>
     ///     <cref>https://www.w3.org/TR/xsl11/#fo_table-row</cref>
     /// </see>
-    public class FoTableRow : FoElement
-    {
+    public class FoTableRow : FoElement {
 
         #region Properties
 
@@ -41,8 +39,7 @@ namespace Limbo.FormattingObjects.Tables
 
         public FoTableRow() { }
 
-        public FoTableRow(params FoTableCell[] cells)
-        {
+        public FoTableRow(params FoTableCell[] cells) {
             Cells.AddRange(cells);
         }
 
@@ -50,61 +47,52 @@ namespace Limbo.FormattingObjects.Tables
 
         #region Member methods
 
-        public FoTableCell AddCell(FoTableCell cell)
-        {
+        public FoTableCell AddCell(FoTableCell cell) {
             Cells.Add(cell);
             return cell;
         }
 
-        public FoTableRow AddCells(IEnumerable<FoTableCell> cells)
-        {
+        public FoTableRow AddCells(IEnumerable<FoTableCell> cells) {
             Cells.AddRange(cells);
             return this;
         }
 
-        public FoTableRow AddCells(params FoTableCell[] cells)
-        {
+        public FoTableRow AddCells(params FoTableCell[] cells) {
             Cells.AddRange(cells);
             return this;
         }
 
-        public FoTableCell CreateCell()
-        {
+        public FoTableCell CreateCell() {
             return AddCell(new FoTableCell());
         }
 
-        public FoTableRow CreateCell(Action<FoTableCell> action)
-        {
+        public FoTableRow CreateCell(Action<FoTableCell> action) {
             FoTableCell cell = new FoTableCell();
             AddCell(cell);
             action(cell);
             return this;
         }
 
-        public FoTableRow CreateCell(out FoTableCell cell)
-        {
+        public FoTableRow CreateCell(out FoTableCell cell) {
             cell = new FoTableCell();
             AddCell(cell);
             return this;
         }
 
-        public FoTableRow CreateCell(out FoTableCell cell, Action<FoTableCell> action)
-        {
+        public FoTableRow CreateCell(out FoTableCell cell, Action<FoTableCell> action) {
             cell = new FoTableCell();
             AddCell(cell);
             action(cell);
             return this;
         }
 
-        protected override void RenderAttributes(XElement element, FoRenderOptions options)
-        {
+        protected override void RenderAttributes(XElement element, FoRenderOptions options) {
             base.RenderAttributes(element, options);
             if (KeepTogetherWithinColumn.HasValue()) element.Add(new XAttribute("keep-together", KeepTogetherWithinColumn));
             if (KeepTogetherWithinColumn.HasValue()) element.Add(new XAttribute("keep-together.within-column", KeepTogetherWithinColumn));
         }
 
-        public override XElement ToXElement(FoRenderOptions options)
-        {
+        public override XElement ToXElement(FoRenderOptions options) {
             XElement element = Fo("table-row");
             RenderAttributes(element, options);
             RenderChildren(element, options);
